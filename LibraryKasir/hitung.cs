38deb670c0cdd33e7f2f7decc241462c4a1470a5
Deavanzack <daffafadillah564@gmail.com>
2024-05-04 21:56:49 +0700

    nambahin class hitung yang isi methodnya transaksi, hitung total untung, pemasukan (itu dulu ya) xixixixi

---
+++ LibraryKasir/hitung.cs
@@ -0,0 +1,28 @@
+﻿namespace LibraryKasir
+{
+        public static class hitung
+        {
+            public static int transaksi(int jumlah, int harga)
+            {
+                return jumlah * harga;
+            }
+
+            public static void pemasukan(int jumlah, int harga, int i, int[] masuk)
+            {
+                int transaksi = hitung.transaksi(jumlah, harga);
+                masuk[i] = transaksi;
+                i = i + 1;
+            }
+
+            public static int keuntungan(int[] masuk, int n, int modal)
+            {
+                int total = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    total = total + masuk[i];
+                }
+
+                return total - modal;
+            }
+        }
+}
