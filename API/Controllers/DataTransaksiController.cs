410418c3382e88f3818fc5cd34409b7e66d01b16
Deavanzack <daffafadillah564@gmail.com>
2024-05-06 12:01:23 +0700

    resolve conflicts

---
+++ API/Controllers/DataTransaksiController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DataTransaksiController : ControllerBase
+    {
+        private static string jsonFilePath = "C:\\Users\\fadillah\\OneDrive - Telkom University\\Dokumen\\My File\\RPL\\SEMESTER 4\\Kontruksi Perangkat Lunak\\ApliKasir\\ApliKasir\\Transaksi.json";
+        private static List<transaksi> transaksi = InitializeDataFromJson(jsonFilePath);
+
+        private static List<transaksi> InitializeDataFromJson(string jsonFilePath)
+        {
+            List<transaksi> data = new List<transaksi>();
+            if (System.IO.File.Exists(jsonFilePath))
+            {
+                string jsonData = System.IO.File.ReadAllText(jsonFilePath);
+                data = JsonConvert.DeserializeObject<List<transaksi>>(jsonData);
+            }
+            return data;
+        }
+
+        private static void SaveDataToJsonFile(List<transaksi> data)
+        {
+            string jsonData = JsonConvert.SerializeObject(data, Formatting.Indented);
+            System.IO.File.WriteAllText(jsonFilePath, jsonData);
+        }
+
+        [HttpGet]
+        public IEnumerable<transaksi> Get()
+        {
+            return transaksi;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] transaksi newvalue)
+        {
+            transaksi.Add(newvalue);
+            SaveDataToJsonFile(transaksi);
+            return Ok(); // Return 200 OK status
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<transaksi> Get(int id)
+        {
+            if (id < 0 || id >= transaksi.Count)
+            {
+                return NotFound(); // Return 404 Not Found status if ID is out of range
+            }
+            return transaksi[id];
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (id < 0 || id >= transaksi.Count)
+            {
+                return NotFound(); // Return 404 Not Found status if ID is out of range
+            }
+            transaksi.RemoveAt(id);
+            SaveDataToJsonFile(transaksi);
+            return NoContent(); // Return 204 No Content status
+        }
+    }
+}
---
+++ LibraryKasir/barang.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryKasir
+{
+    public static class barang
+    {
+        public struct brg
+        {
+            public string nama;
+            public int harga;
+            public int stok;
+            public int id;
+        }
+        public static void InsertBarang(int id, string nama, int harga, int stok, brg[] brg, int idx)
+        {
+            brg[idx].id = idx;
+            brg[idx].nama = nama;
+            brg[idx].harga = harga;
+            brg[idx].stok = stok;
+            idx++;
+        }
+
+        public static int cariidxbarang(brg[] brg, string nama)
+        {
+            int i = 0;
+            bool ketemu = false;
+            while (i <= brg.Length && ketemu == false)
+            {
+                if (brg[i].nama == nama)
+                {
+                    ketemu = true;
+                } else
+                {
+                    i++;
+                }
+            }
+            if (ketemu = false)
+            {
+                return -1;
+            } else
+            {
+                return i;
+            }
+        }
+    }
+}
