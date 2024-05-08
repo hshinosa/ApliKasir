b1f2af31850930705fc20a04ca4e733b336bc4be
hshinosa <mhashfi12@gmail.com>
2024-05-08 11:33:32 +0700

    Mengimplementasikan Menu ke Program

--- ApliKasir/DriverKasir.cs
+++ ApliKasir/DriverKasir.cs
@@ -1,83 +1,134 @@
-﻿using static Menu;
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using LibraryKasir;
 
-public class Menu
+internal class Program
 {
-    public enum NamaMenu
+    static async Task Main(string[] args)
     {
-        InputDataBarang,
-        InputDataTransaksi,
-        InputDataHutang,
-        TampilkanDataBarang,
-        TampilkanDataTransaksi,
-        TampilkanDataHutang,
-        HapusDataBarang,
-        HapusDataTransaksi,
-        HapusDataHutang,
-        Keluar
-    }
+        string baseUrl = "https://localhost:7058";
 
-    private static Dictionary<NamaMenu, string> kodeMenuTabel = new Dictionary<NamaMenu, string>
-    {
-        { NamaMenu.InputDataBarang, "1" },
-        { NamaMenu.InputDataTransaksi, "2" },
-        { NamaMenu.InputDataHutang, "3" },
-        { NamaMenu.TampilkanDataBarang, "4" },
-        { NamaMenu.TampilkanDataTransaksi, "5" },
-        { NamaMenu.TampilkanDataHutang, "6" },
-        { NamaMenu.HapusDataBarang, "7" },
-        { NamaMenu.HapusDataTransaksi, "8" },
-        { NamaMenu.HapusDataHutang, "9" },
-        { NamaMenu.Keluar, "10" }
-    };
-
-    private static Dictionary<string, NamaMenu> menuKodeTabel = new Dictionary<string, NamaMenu>
-    {
-        { "1", NamaMenu.InputDataBarang},
-        { "2", NamaMenu.InputDataTransaksi},
-        { "3", NamaMenu.InputDataHutang},
-        { "4", NamaMenu.TampilkanDataBarang},
-        { "5", NamaMenu.TampilkanDataTransaksi},
-        { "6", NamaMenu.TampilkanDataHutang},
-        { "7", NamaMenu.HapusDataBarang},
-        { "8", NamaMenu.HapusDataTransaksi},
-        { "9", NamaMenu.HapusDataHutang},
-        { "10", NamaMenu.Keluar}
-    };
-
-    public static string getKodemenu(NamaMenu menu)
-    {
-        return kodeMenuTabel.GetValueOrDefault(menu, "Kode menu tidak ditemukan");
-    }
-
-    public static NamaMenu? getNamaMenu(string kode)
-    {
-        if (menuKodeTabel.TryGetValue(kode, out NamaMenu namaMenu))
+        static LibraryKasir.Menu.NamaMenu GetMenuChoice()
         {
-            return namaMenu;
+            Console.Write("Masukkan pilihan: ");
+            string choice = Console.ReadLine();
+            return LibraryKasir.Menu.getNamaMenu(choice).Value;
         }
-        else
+
+        while (true)
         {
-            return null;
-        }
-    }
+            LibraryKasir.Menu.DisplayMenu();
+            LibraryKasir.Menu.NamaMenu choice = GetMenuChoice();
+
+            switch (choice)
+            {
+                case Menu.NamaMenu.InputDataBarang:
+                    // Data Barang
+                    Console.WriteLine("Data Barang");
+                    Console.Write("Masukkan ID Barang : ");
+                    int idBarang = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Masukkan Nama Barang : ");
+                    string namaBarang = Console.ReadLine();
+                    Console.Write("Masukkan Harga Barang : ");
+                    double hargaBarang = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Masukkan Jumlah Barang : ");
+                    int jumlahBarang = Convert.ToInt32(Console.ReadLine());
+
+                    LibraryKasir.DataBarang barang = new LibraryKasir.DataBarang
+                    {
+                        idBarang = idBarang,
+                        namaBarang = namaBarang,
+                        hargaBarang = hargaBarang,
+                        jumlahBarang = jumlahBarang
+                    };
+
+                    await LibraryKasir.Barang.CreateBarang(baseUrl, barang);
+                    break;
+                case Menu.NamaMenu.InputDataTransaksi:
+                    // Data Transaksi
+                    Console.WriteLine("Data Transaksi");
+                    Console.Write("Masukkan ID Transaksi : ");
+                    int idTransaksi = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Masukkan Nama Barang : ");
+                    namaBarang = Console.ReadLine();
+                    Console.Write("Masukkan Jumlah Barang : ");
+                    jumlahBarang = Convert.ToInt32(Console.ReadLine());
+
+                    // Retrieve the hargaBarang from the DataBarang data
+                    hargaBarang = await Hitung.GetHargaBarang(baseUrl, namaBarang);
 
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Masukkan kode menu: ");
-            string kodeMenu = Console.ReadLine();
+                    LibraryKasir.DataTransaksi transaksi = new LibraryKasir.DataTransaksi
+                    {
+                        idTransaksi = idTransaksi,
+                        namaBarang = namaBarang,
+                        jumlahBarang = jumlahBarang,
+                        totalHarga = totalHarga
+                    };
+
+                    await LibraryKasir.Hitung.CreateTransaksi(baseUrl, transaksi);
+                    break;
+                case Menu.NamaMenu.InputDataHutang:
+                    // Data Hutang
+                    Console.WriteLine("Data Hutang");
+                    Console.Write("Masukkan ID Hutang : ");
+                    int idHutang = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Masukkan Nama Pelanggan : ");
+                    string namaPelanggan = Console.ReadLine();
+                    Console.Write("Masukkan Nama Barang : ");
+                    namaBarang = Console.ReadLine();
+                    Console.Write("Masukkan Jumlah Barang : ");
+                    jumlahBarang = Convert.ToInt32(Console.ReadLine());
+
+                    // Retrieve the hargaBarang from the DataBarang data
+                    hargaBarang = await LibraryKasir.Hitung.GetHargaBarang(baseUrl, namaBarang);
 
             NamaMenu? NamaMenu = Menu.getNamaMenu(kodeMenu);
 
-            if (NamaMenu == null)
-            {
-                Console.WriteLine("Input salah, input ulang.");
-            }
-            else
-            {
-                Console.WriteLine($"Nama menu: {NamaMenu}");
+                    LibraryKasir.DataHutang hutang = new LibraryKasir.DataHutang
+                    {
+                        idHutang = idHutang,
+                        namaPelanggan = namaPelanggan,
+                        namaBarang = namaBarang,
+                        jumlahBarang = jumlahBarang,
+                        totalHarga = totalHarga
+                    };
+
+                    await LibraryKasir.Hutang.CreateHutang(baseUrl, hutang);
+                    break;
+                case Menu.NamaMenu.TampilkanDataBarang:
+                    await LibraryKasir.Display.DisplayData(baseUrl, "/DataBarang");
+                    break;
+                case Menu.NamaMenu.TampilkanDataTransaksi:
+                    await LibraryKasir.Display.DisplayData(baseUrl, "/DataTransaksi");
+                    break;
+                case Menu.NamaMenu.TampilkanDataHutang:
+                    await LibraryKasir.Display.DisplayData(baseUrl, "/DataHutang");
+                    break;
+                case Menu.NamaMenu.HapusDataBarang:
+                    await LibraryKasir.Display.DisplayData(baseUrl, "/DataBarang");
+                    Console.WriteLine("Masukkan ID Barang : ");
+                    idBarang = Convert.ToInt32(Console.ReadLine());
+                    await LibraryKasir.Barang.DeleteBarang(baseUrl, idBarang);
+                    break;
+                case Menu.NamaMenu.HapusDataTransaksi:
+                    await LibraryKasir.Display.DisplayData(baseUrl, "/DataTransaksi");
+                    Console.WriteLine("Masukkan ID Transaksi : ");
+                    idTransaksi = Convert.ToInt32(Console.ReadLine());
+                    await LibraryKasir.Barang.DeleteBarang(baseUrl, idTransaksi);
+                    break;
+                case Menu.NamaMenu.HapusDataHutang:
+                    await LibraryKasir.Display.DisplayData(baseUrl, "/DataHutang");
+                    Console.WriteLine("Masukkan ID Hutang : ");
+                    idHutang = Convert.ToInt32(Console.ReadLine());
+                    await LibraryKasir.Barang.DeleteBarang(baseUrl, idHutang);
+                    break;
+                case Menu.NamaMenu.Keluar:
+                    Console.WriteLine("Terima kasih telah menggunakan!");
+                    return;
+                default:
+                    Console.WriteLine("Pilihan tidak valid. Silakan coba lagi.");
+                    break;
             }
         }
     }
